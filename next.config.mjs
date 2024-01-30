1617c0101b6a65de6bccbc1c38dcf2447144c08a
Dmitry Fuks <d.fuks@dodopizza.com>
2024-01-30 10:13:43 +0300

    fix open api key

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,6 @@ const nextConfig = {
         '@ant-design/pro-editor',
         'react-intersection-observer',
     ],
-    env: {
-        OPENAI_KEY: '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',
-    }
 };
 
 export default nextConfig;
